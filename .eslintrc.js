018089649686a5dbdc3083180208dcbe4494d962
Jisu <hellojs242@gmail.com>
2024-08-28 13:25:11 +0900

    :hammer: chore: Eslint, prettier 설정 #3
    - 이전 social-feed-hub 설정에 import 순서 옵션을 추가했습니다.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,21 +5,50 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'plugin:import/recommended'],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', '**/*.constants.ts', '**/*.decorator.ts'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'func-style': ['error', 'expression'],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      { selector: 'variableLike', format: ['camelCase'] },
+      { selector: 'property', format: ['camelCase'] },
+      { selector: 'class', format: ['PascalCase'] },
+      { selector: 'classMethod', format: ['camelCase'] },
+      { selector: 'objectLiteralMethod', format: ['camelCase'] },
+    ],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    '@typescript-eslint/explicit-function-return-type': ['warn', { allowExpressions: true }], // 함수의 반환 타입 명시
+    '@typescript-eslint/no-explicit-any': 'error', // any 사용시 경고
+    'no-else-return': 'warn', // else 뒤에 바로 return 경고
+    'import/order': [
+      // import 순서를 정렬해줍니다.
+      'error',
+      {
+        alphabetize: {
+          order: 'asc', //alphaber 순서로 정렬
+        },
+        'newlines-between': 'always', // 그룹 별 newline 으로 구분
+        //그룹은 기본 ["builtin", "external", "parent", "sibling", "index"] 순서입니다. 변경 가능
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        moduleDirectory: ['node_modules', 'src'],
+        extensions: ['.js', '.ts'],
+      },
+    },
   },
 };
