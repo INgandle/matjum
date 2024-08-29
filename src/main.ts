9181cc8658e79b6977be2ace4c508aaf41434543
Jisu <hellojs242@gmail.com>
2024-08-29 17:04:27 +0900

    :hammer: chore: swagger 설정 #11

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
+const config = new DocumentBuilder()
+  .setTitle('Matjum API')
+  .setDescription('맛점: 위치기반 맛집 추천 서비스의 API 문서입니다.')
+  .setVersion('1.0')
+  .addTag('Matjum')
+  .build();
+
 const bootstrap = async (): Promise<void> => {
   const app = await NestFactory.create(AppModule);
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 };
 
