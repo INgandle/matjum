27e776c8b081e9a995d4de13f56955682a0e9465
ooheunda <ooheunda@gmail.com>
2024-08-31 12:10:41 +0900

    :sparkles: feat: 사용자 정보 조회, 설정 업데이트 API에 스웨거 적용 #10 #12

--- src/members/dto/update-member.dto.ts
+++ src/members/dto/update-member.dto.ts
@@ -1,14 +1,26 @@
 import { IsBoolean, IsLatitude, IsLongitude, IsNotEmpty } from 'class-validator';
 
 export class UpdateMemberDto {
+  /**
+   * 사용자의 위도
+   * @example 37.564625
+   */
   @IsLatitude()
   @IsNotEmpty()
   lat: number;
 
+  /**
+   * 사용자의 경도
+   * @example 126.977260
+   */
   @IsLongitude()
   @IsNotEmpty()
   lon: number;
 
+  /**
+   * 추천 기능 사용 여부
+   * @example true
+   */
   @IsBoolean()
   @IsNotEmpty()
   isRecommendationEnabled: boolean;
--- src/members/members.controller.ts
+++ src/members/members.controller.ts
@@ -1,18 +1,30 @@
 import { Controller, Get, Body, Patch, Param, HttpStatus, HttpCode } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 import { MemberResponseDto } from './dto/member-response.dto';
 import { UpdateMemberDto } from './dto/update-member.dto';
 import { MembersService } from './members.service';
 
 @Controller('members')
+@ApiTags('members')
 export class MembersController {
   constructor(private readonly membersService: MembersService) {}
 
+  /**
+   * 사용자 정보 조회 API
+   * @param id 사용자 PK
+   * @returns 패스워드를 제외한 모든 사용자 정보
+   */
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<MemberResponseDto> {
     return await this.membersService.findOne(id);
   }
 
+  /**
+   * 사용자 설정 업데이트 API
+   * @param id 사용자 PK
+   * @param updateMemberDto 업데이트 할 정보
+   */
   @Patch(':id/settings')
   @HttpCode(HttpStatus.NO_CONTENT)
   updateSettings(@Param('id') id: string, @Body() updateMemberDto: UpdateMemberDto): void {
