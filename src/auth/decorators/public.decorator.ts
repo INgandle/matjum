81ce95d642fd44cdcdbf603ea7b8a840f3bd8da7
joosomi <wnthal1211@gmail.com>
2024-09-01 14:59:59 +0900

    ♻️ refactor: JwtAuthGuard에 제네릭 타입 추가 (#16)
    - lint 규칙 체크하여 수정
    - handleRequest 메서드에 제네릭 타입 TUser를 추가하여 타입 안전성과 유연성 향상.
    - canActivate 메서드의 반환 타입을 명시적으로 정의.

--- src/auth/decorators/public.decorator.ts
+++ src/auth/decorators/public.decorator.ts
@@ -1,4 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
 
-export const IS_PUBLIC_KEY = 'isPublic';
-export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+export const IsPublicKey = 'isPublic';
+export const Public = (): ReturnType<typeof SetMetadata> => SetMetadata(IsPublicKey, true);
--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,8 +1,10 @@
 import { Injectable, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
-import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+import { IsPublicKey } from '../decorators/public.decorator';
+import { JwtUser } from '../strategies/jwt.types';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
@@ -10,21 +12,20 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     super(); //AuthGuard('jwt') 기능을 상속 받음
   }
 
-  canActivate(context: ExecutionContext) {
-    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IsPublicKey, [context.getHandler(), context.getClass()]);
     if (isPublic === true) {
       return true;
     }
     return super.canActivate(context); //jwt 유효성 검사
   }
 
-  handleRequest(err, user) {
+  // TUser 제네릭을 사용하여 기본값을 JwtUser로 설정
+  // JwtUser 타입을 기본으로 사용하면서도 필요에 따라 다른 타입 지원 가능
+  handleRequest<TUser extends JwtUser = JwtUser>(err: Error, user: TUser | false): TUser {
     if (err || !user) {
       throw new UnauthorizedException('Authentication failed');
     }
-    return user;
+    return user as TUser;
   }
 }
