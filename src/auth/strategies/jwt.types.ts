cbd26203a24f48f2084e413841cc12aeb418e533
joosomi <wnthal1211@gmail.com>
2024-09-02 13:47:21 +0900

    ♻️ refactor: Payload, User interface에서 type으로 통일

--- src/auth/strategies/jwt.types.ts
+++ src/auth/strategies/jwt.types.ts
@@ -1,12 +1,11 @@
-// 향후 확장 가능성(예: 추가 필드)을 고려할 때 interface 선언
-export interface JwtPayload {
+export type JwtPayload = {
   sub: string;
   accountName: string;
   name: string;
-}
+};
 
-export interface JwtUser {
+export type JwtUser = {
   id: string;
   accountName: string;
   name: string;
-}
+};
