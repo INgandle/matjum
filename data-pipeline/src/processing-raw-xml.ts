8331a4ae03f85b02ae0d80a763740212b1d5fe24
Jisu <hellojs242@gmail.com>
2024-09-02 12:48:03 +0900

    :recycle: refactor: 전역변수 제거 및 ProcessedData를 더 명확한 타입으로 변경 #19
    - x, y 대신 location 함수를 바로 processing에 삽입하여 반복횟수를 줄였습니다.
    - 전역에서 사용되던 XMLparser를 readMultipleFiles로 옮겨 생명주기를 변경했습니다.
    - 필요없는 주석 삭제

--- data-pipeline/src/processing-raw-xml.ts
+++ data-pipeline/src/processing-raw-xml.ts
@@ -1,5 +1,5 @@
 /**
- * xml 허가 파일들을 처리하는 script
+ * localdata.kr 에서 download 받은 xml 파일들을 처리하는 script
  * xml 파일을 읽어서 필요한 데이터만 추출하여 가공한 후 반환한다.
  */
 
@@ -12,52 +12,46 @@ import { excludeCategorySet } from './common/common.constants';
 import { ProcessedData } from './types/processed-data.type';
 import { RawData } from './types/raw-data.type';
 
-// 전역 변수
-const options: X2jOptions = {
-  ignoreAttributes: true,
-};
-
-const parser = new XMLParser(options);
-
 /**
  * 도로명, 지번 주소를 합쳐서 반환
  *
- * @param siteWhlAddr
- * @param rdnWhlAddr
+ * @param siteWhlAddr 지번 주소
+ * @param rdnWhlAddr 도로명 주소
  */
-const formatAddress = (siteWhlAddr: string, rdnWhlAddr: string): string => {
-  let address = '';
+const formatAddress = (siteWhlAddr?: string, rdnWhlAddr?: string): string | null => {
+  // undefined, ''이면 null로 처리
+  const validRdnWhlAddr = rdnWhlAddr && rdnWhlAddr !== '' ? rdnWhlAddr : null;
+  const validSiteWhlAddr = siteWhlAddr && siteWhlAddr !== '' ? siteWhlAddr : null;
 
-  if (rdnWhlAddr !== '' && rdnWhlAddr !== undefined && rdnWhlAddr !== null) {
-    address += rdnWhlAddr;
-    if (siteWhlAddr !== '' && siteWhlAddr !== undefined && siteWhlAddr !== null) {
-      address += ` (${siteWhlAddr})`;
-    }
-  } else {
-    address += siteWhlAddr;
+  // 두 주소가 모두 유효하면 도로명 주소 (지번 주소) 형식으로 반환
+  if (validRdnWhlAddr && validSiteWhlAddr) {
+    return `${validRdnWhlAddr} (${validSiteWhlAddr})`;
   }
 
-  return address;
+  // 둘 중 하나만 유효하면 유효한 주소만 반환, 둘 다 유효하지 않으면 null 반환
+  return validRdnWhlAddr || validSiteWhlAddr;
 };
 
 /**
  * 데이터를 가공하여 반환
- * undefined 를 이용해 name, address가 겹치는 경우에 ''이 아닌 필드만 업데이트 한다.
+ * ''인 필드는 null 로 처리.
  *
  * @param data
  * @returns
  */
-const dataFormatting = (data: RawData[]): Partial<ProcessedData>[] => {
-  const processed: Partial<ProcessedData>[] = data.reduce((acc: ProcessedData[], item: RawData) => {
+const dataFormatting = (data: RawData[]): ProcessedData[] => {
+  const processed: ProcessedData[] = data.reduce((acc: ProcessedData[], item: RawData) => {
     // 제외 카테고리에 포함되지 않는 항목만 처리
     if (!excludeCategorySet.has(item.uptaeNm)) {
       acc.push({
-        name: item.bplcNm,
+        name: item.bplcNm, // not null
         address: formatAddress(item.siteWhlAddr, item.rdnWhlAddr),
-        category: item.uptaeNm === '' ? undefined : item.uptaeNm,
-        phoneNumber: item.siteTel === '' ? undefined : item.siteTel,
-        x: item.x === '' ? undefined : item.x,
-        y: item.y === '' ? undefined : item.y,
+        category: item.uptaeNm === '' ? null : item.uptaeNm,
+        phoneNumber: item.siteTel === '' ? null : item.siteTel,
+        location:
+          item.x && item.y
+            ? () => `ST_SetSRID(ST_Transform(ST_SetSRID(ST_MakePoint(${item.x}, ${item.y}), 5174), 4326), 4326)`
+            : null,
         lastModTs: item.lastModTs,
       });
     }
@@ -73,11 +67,13 @@ const dataFormatting = (data: RawData[]): Partial<ProcessedData>[] => {
  * @param filePath
  * @returns
  */
-const readFile = async (filePath: string): Promise<Partial<ProcessedData>[]> => {
+const readFile = async (parser: XMLParser, filePath: string): Promise<ProcessedData[]> => {
   try {
     const content = await fs.readFile(filePath, 'utf-8');
     console.log(`Successfully read file: ${filePath}`);
-    return dataFormatting(parser.parse(content).result.body.rows.row);
+
+    const parsedData = parser.parse(content).result.body.rows.row;
+    return dataFormatting(parsedData);
   } catch (error) {
     console.error(`Error reading file ${filePath}:`, error);
     throw error;
@@ -90,12 +86,19 @@ const readFile = async (filePath: string): Promise<Partial<ProcessedData>[]> => 
  * @param fileNames
  * @returns
  */
-const readMultipleFiles = async (fileNames: string[]): Promise<Partial<ProcessedData>[]> => {
+const readMultipleFiles = async (fileNames: string[]): Promise<ProcessedData[]> => {
   const filePaths = fileNames.map((fileName) => join(__dirname, '..', 'data', fileName));
+
+  // 전역 변수
+  const options: X2jOptions = {
+    ignoreAttributes: true,
+  };
+
+  const parser = new XMLParser(options);
 
   try {
     // Promise.all을 사용하여 병렬로 처리
-    const fileContents = await Promise.all(filePaths.map(readFile));
+    const fileContents = await Promise.all(filePaths.map((filePath) => readFile(parser, filePath)));
     console.log('All files have been read successfully');
     return fileContents.flat();
   } catch (error) {
@@ -106,9 +109,9 @@ const readMultipleFiles = async (fileNames: string[]): Promise<Partial<Processed
 
 /**
  * xml 파일들을 읽어 가공하여 반환
- * @returns
+ * @returns ProcessedData 배열
  */
-const processingRawXML = async (): Promise<Partial<ProcessedData>[]> => {
+const processingRawXML = async (): Promise<ProcessedData[]> => {
   // TODO: 파일명 정리
   const fileNames = [
     'seoul_20200801-20240831.xml',
--- data-pipeline/src/types/processed-data.type.ts
+++ data-pipeline/src/types/processed-data.type.ts
@@ -16,8 +16,7 @@ export type ProcessedData = {
   name: string; // 사업장명 - bpclNm
   address: string; // 도로명(지번) 형태의 주소 - siteWhlAddr, rdnWhlAddr
   category: string; // 업태구분명 - uptaeNm
-  x: string; // 좌표정보(X) - x - 특이사항 : 중부원점TM(EPSG:5174) 사용
-  y: string; // 좌표정보(Y) - y
+  location: () => string; // 좌표정보 계산하는 function
   phoneNumber: string; // 전화번호 - siteTel
   lastModTs: string; // 최종수정일자 - lastModTs
 };
